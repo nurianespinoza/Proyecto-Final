d5396255c98c802db1e4e3ecdf832b662cc920e9
nurianespinoza <nurianespinoza6@gmail.com>
2024-05-01 20:10:23 -0600

    añadiendo el scroll view

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState } from "react";
 import {
   StyleSheet,
@@ -10,6 +8,7 @@ import {
   Modal,
   TextInput,
   Linking,
+  ScrollView,
 } from "react-native";
 
 const UserProfile = ({
@@ -23,19 +22,18 @@ const UserProfile = ({
   location: string;
   description: string;
   imageUrl: string;
-  links: { url: string; text: string }[];
+  links?: { url: string; text: string }[];
 }) => {
   const [modalVisible, setModalVisible] = useState(false);
   const [newLink, setNewLink] = useState({ url: "", text: "" });
 
   const handleAddLinkPress = () => {
     setModalVisible(true);
-    
   };
 
   const handleSaveLinkPress = () => {
     if (newLink.url && newLink.text) {
-      links.push({ url: newLink.url, text: newLink.text });
+      links?.push({ url: newLink.url, text: newLink.text });
       setModalVisible(false);
       setNewLink({ url: "", text: "" });
     }
@@ -58,8 +56,8 @@ const UserProfile = ({
             <Text style={styles.locationText}>{location}</Text>
             <Text style={styles.descriptionText}>{description}</Text>
           </View>
-          <View style={styles.linksContainer}>
-            {links.map((link, index) => (
+          <ScrollView style={styles.linksContainer}>
+            {links?.map((link, index) => (
               <TouchableOpacity
                 key={index}
                 style={styles.linkButton}
@@ -68,10 +66,10 @@ const UserProfile = ({
                 <Text style={styles.linkText}>{link.text}</Text>
               </TouchableOpacity>
             ))}
-            <TouchableOpacity style={styles.addLinkButton} onPress={handleAddLinkPress}>
-              <Text style={styles.addLinkText}>+ New Link</Text>
-            </TouchableOpacity>
-          </View>
+          </ScrollView>
+          <TouchableOpacity style={styles.addLinkButton} onPress={handleAddLinkPress}>
+            <Text style={styles.addLinkText}>+ New Link</Text>
+          </TouchableOpacity>
         </View>
       </View>
       <Modal
@@ -97,7 +95,8 @@ const UserProfile = ({
               value={newLink.text}
               onChangeText={(text) => setNewLink({ ...newLink, text: text })}
             />
-            <View style={styles.buttonContainer}><TouchableOpacity style={styles.cancelButton} onPress={handleCancelLinkPress}>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.cancelButton} onPress={handleCancelLinkPress}>
                 <Text style={styles.cancelText}>Cancel</Text>
               </TouchableOpacity>
               <TouchableOpacity style={styles.saveButton} onPress={handleSaveLinkPress}>
@@ -142,6 +141,14 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     overflow: "hidden",
     marginRight: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   profileImage: {
     width: "100%",
@@ -171,8 +178,7 @@ const styles = StyleSheet.create({
   },
   linksContainer: {
     marginTop: -20,
-    alignItems: "center",
-    justifyContent: "center",
+    maxHeight: 300, // Ajusta según tus necesidades
   },
   linkButton: {
     backgroundColor: "#333333",
@@ -181,7 +187,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginBottom: 25,
     height: 52,
-    width: 300, // Establece el ancho aquí
+    width: 300, 
     alignItems: "center",
     justifyContent: "center",
   },
@@ -196,7 +202,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 30,
     height: 60,
-    width: 300, // Establece el ancho aquí
+    width: 300, 
     alignItems: "center",
     justifyContent: "center",
   },
@@ -213,7 +219,7 @@ const styles = StyleSheet.create({
   modalContent: {
     backgroundColor: "#fff",
     padding: 20,
-    borderRadius: 10,
+    borderRadius: 5,
     width: "100%",
     maxWidth: 300,
   },
@@ -224,7 +230,7 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    borderColor: "#ccc",
+    borderColor: "#000",
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
@@ -240,19 +246,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#FF0000",
     paddingVertical: 8,
     paddingHorizontal: 16,
-    borderRadius: 20,
+    borderRadius: 5,
   },
   cancelText: {
     fontSize: 16,
-    color: "#333",
+    color: "#fff",
   },
   saveButton: {
-    backgroundColor: "#0AFF",
+    backgroundColor: "#A367FA",
     paddingVertical: 8,
     paddingHorizontal: 16,
-    borderRadius: 20,
+    borderRadius: 5,
   },
-  saveText:{fontSize: 16,
+  saveText: {
+    fontSize: 16,
     color: "#fff",
   },
 });
