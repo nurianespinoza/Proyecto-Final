42c743c15f51a429e76008c6c41f8bff6e637b03
nurianespinoza <nurianespinoza6@gmail.com>
2024-05-01 20:05:40 -0600

    Modificando archivo app

--- App.tsx
+++ App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import UserProfile from './components/UserProfile'
+import UserProfile from './components/UserProfile';
 
 const App: React.FC = () => {
-  const userProfileData = {
-    name: 'Jessica Randall',
-    location: 'London, United Kingdom',
-    description: '"Front-end developer and avid reader".',
-    imageUrl: "https://automationhero.ai/wp-content/uploads/2020/01/Jessica-Randall.png",
-    links: [
-      { url: 'https://github.com/nurianespinoza', text: 'GitHub' },
-      { url: 'https://www.frontendmentor.io/', text: 'Frontend Mentor' },
-      { url: 'https://www.linkedin.com/feed/', text: 'LinkedIn' },
-      { url: 'https://twitter.com/?lang=es', text: 'Twitter' },
-    ],
-  };
-
   return (
     <View style={styles.container}>
-      <UserProfile {...userProfileData} />
+      <UserProfile 
+        name="Jessica Randall"
+        location="London, United Kingdom"
+        description='"Front-end developer and avid reader".'
+        imageUrl="https://automationhero.ai/wp-content/uploads/2020/01/Jessica-Randall.png"
+        links={[
+          { url: 'https://github.com/nurianespinoza', text: 'GitHub' },
+          { url: 'https://www.frontendmentor.io/', text: 'Frontend Mentor' },
+          { url: 'https://www.linkedin.com/feed/', text: 'LinkedIn' },
+          { url: 'https://twitter.com/?lang=es', text: 'Twitter' }
+        ]}
+      />
     </View>
   );
 };
@@ -26,14 +24,10 @@ const App: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0000',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
 
 export default App;
-
-
-
-
