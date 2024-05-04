2cce57a306fa03b5f3448792aafb6a9d218fe7bb
nurianespinoza <nurianespinoza6@gmail.com>
2024-05-04 09:11:31 -0600

    componiendo mejoras en el howws

--- hoows/HoowsUseModalPost.tsx
+++ hoows/HoowsUseModalPost.tsx
@@ -1,36 +1,3 @@
-// import { useState } from 'react';
-
-// interface UseModalProps {
-//   initialVisible: boolean;
-//   initialLink: { url: string; text: string };
-// }
-
-// interface UseModalReturn {
-//   modalVisible: boolean;
-//   newLink: { url: string; text: string };
-//   setModalVisible: (visible: boolean) => void;
-//   setNewLink: (link: { url: string; text: string }) => void;
-// }
-
-// export const useModal = (props: UseModalProps): UseModalReturn => {
-//   const { initialVisible, initialLink } = props;
-//   const [modalVisible, setModalVisible] = useState(initialVisible);
-//   const [newLink, setNewLink] = useState(initialLink);
-
-//   const handleModalVisible = (visible: boolean) => {
-//     setModalVisible(visible);
-//     if (!visible) {
-//       setNewLink(initialLink);
-//     }
-//   };
-
-//   return {
-//     modalVisible,
-//     newLink,
-//     setModalVisible: handleModalVisible,
-//     setNewLink,
-//   };
-// };
 
 import { useEffect, useState } from "react";
 
@@ -51,7 +18,7 @@ export const usePosts = () => {
     const [isLoading, setIsLoading] = useState(true);
 
     const fetchPosts = async () => {
-        const response = await fetch("http://192.168.1.8:3001/");
+        const response = await fetch("http://192.168.1.11:3001/");
         const data: Array<Post> = await response.json();
 
         setData(data);
